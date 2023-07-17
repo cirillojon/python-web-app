d8e3c3eb8214ffde92f69d3efec70c44b7ec5462
Jon <jonathancirillo@outlook.com>
2023-07-17 12:09:34 -0400

    added login manager

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
                 if check_password_hash(user.password, password):
                     flash('Logged in successfully!', category='success')
+                    login_user(user, remember=True)
+                    return redirect(url_for('views.home'))
                 else:
                     flash('Incorrect password, try again.', category='error')
         else:
@@ -24,8 +27,10 @@ def login():
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -52,6 +57,7 @@ def sign_up():
                 db.session.add(new_user)
                 db.session.commit()
                 flash('Account created!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             except:
                 flash('Issue with Sign Up', category='error')
