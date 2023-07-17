691d350f6c6c5c41ff458dda1a53671f48fa8602
Jon <jonathancirillo@outlook.com>
2023-07-17 12:47:30 -0400

    working delete note

--- website/static/index.js
+++ website/static/index.js
@@ -1,2 +1,10 @@
 // All images, js, or css files will go into static folder
 
+function deleteNote(noteId) {
+    fetch('/delete-note', {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId})
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
